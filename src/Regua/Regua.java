fe750c01ac1f680a1d3850d20fdffd351ecbb297
Ocirne <enrico.guimaraes1103@gmail.com>
2026-08-27 10:04:03 -0300

    adicionando atividades: um bucado

--- src/Regua/Regua.java
+++ src/Regua/Regua.java
@@ -1,20 +1,6 @@
 package Regua;
 
 public class Regua {
-    public void regua(int esquerda, int direita, int altura) {
-        if(altura <= 0) {
-            return;
-        }
-        /*
-        if(altura != 0) {
-            ...comandos...
-        }
-         */
-        int media = (esquerda + direita) / 2;
-        marca(media,  altura);
-    }
 
-    public void marca(int media, int altura) {
-        return;
-    }
+
 }
